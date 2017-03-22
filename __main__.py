c0e3ab4b5af3d3acf7d832f1d5a2bb63b86e1a16
Sekalf Nroc <sekalf.256@gmail.com>
2017-03-22 19:07:06 +0000

    1.0.0 Filtered < 5
    Filtered out all numbers less than five and printed the new array.

--- __main__.py
+++ __main__.py
@@ -1,3 +1,10 @@
-#!/usr/bin/env python3
-a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+#!/usr/bin/env python
+numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+
+less_than = []
+for number in numbers:
+    if number < 5:
+        less_than.append(str(number))
+
+print("The numbers in the list less than 5 are %s" % ", ".join(less_than))
 
